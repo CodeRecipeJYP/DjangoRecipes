47109296068cc9bbbb95aa9a123e319cb7c95e38
pjo901018 <pjo901018@korea.ac.kr>
2017-04-23 01:38:23 +0900

    html extends, staticfiles

--- ALCWeb_pyv2/urls.py
+++ ALCWeb_pyv2/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from api import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^api/', include("api.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
