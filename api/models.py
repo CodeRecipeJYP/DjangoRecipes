4f1f83052b8ee6bbad54c2ed4a25585e1b06e705
pjo901018 <pjo901018@korea.ac.kr>
2017-04-22 22:20:19 +0900

    add Course

--- api/models.py
+++ api/models.py
@@ -18,3 +18,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+
+class Course(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
