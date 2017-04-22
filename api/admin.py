eafa2924942c8f079e5a8ac83de215b1b4696e94
pjo901018 <pjo901018@korea.ac.kr>
2017-04-23 01:48:46 +0900

    StackedInline adminpage

--- api/admin.py
+++ api/admin.py
@@ -3,8 +3,14 @@ from api.models import Post, Comment, Course, Step
 
 # Register your models here.
 
+class StepInline(admin.StackedInline):
+    model = Step
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [StepInline, ]
+
 
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Step)
