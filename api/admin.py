2a6d20bc50d6741a479e5700c97cd580cf606c4d
pjo901018 <pjo901018@korea.ac.kr>
2017-04-23 00:08:59 +0900

    Course list url생성

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from api.models import Post, Comment
+from api.models import Post, Comment, Course
 
 # Register your models here.
 
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Course)
--- api/views.py
+++ api/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from api.models import Course
+
+
 def index(request):
     return HttpResponse("Hello world")
+
+def course_list(request):
+    courses = Course.objects.all()
+    output = ', '.join([str(course) for course in courses])
+    return HttpResponse(output)
