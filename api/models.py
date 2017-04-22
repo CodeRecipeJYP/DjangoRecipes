2694c649e888e5c4eb67c1c5eda6fb3e40632fb6
pjo901018 <pjo901018@korea.ac.kr>
2017-04-22 22:09:25 +0900

    copy from ALCWeb-py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', default=1)
+    title = models.CharField(null=False, blank=False, max_length=256)
+    content = models.TextField(null=False, blank=False, max_length=2048)
+    uploaded_date = models.DateTimeField(auto_created=True, auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('api.Post', related_name='comments')
+    author = models.ForeignKey('auth.User', default=1)
+    content = models.CharField(null=False, blank=False, max_length=256)
+    uploaded_date = models.DateTimeField(auto_created=True, auto_now=True)
+
+    def __str__(self):
+        return self.content
